b4ced71de5277d7cc5861e9164428b5046f20ea5
Stav Levi <stav.levi@netnut.io>
2024-08-20 15:16:23 +0300

    remove commands

--- client/src/test/order.submit.reset.metadata.test.ts
+++ client/src/test/order.submit.reset.metadata.test.ts
@@ -5,38 +5,32 @@ test("form submission displays results in the correct order", async ({
 }) => {
   await page.goto("http://localhost:5174");
 
-  // המתנה שהאלמנטים של הטופס ייטענו
   await page.waitForSelector('input[placeholder="Enter Url"]', {
     timeout: 10000,
   });
 
-  // הזנת כתובות URL בטופס
   await page.fill('input[name="url1"]', "https://www.google.com");
   await page.fill('input[name="url2"]', "https://www.facebook.com");
   await page.fill('input[name="url3"]', "https://www.twitter.com");
 
-  // לחיצה על כפתור ההגשה
   await page.click('button[type="submit"]');
 
-  // המתנה להצגת התוצאות
   await page.waitForSelector("a", { timeout: 10000 });
 
-  // בדיקת סדר התוצאות
   const metadataUrls = await page.$$eval("a", (elements) =>
     elements.map((element) => element.textContent)
   );
 
-  expect(metadataUrls.length).toBe(3); // ושזה הסדר בדוק שיש שלוש תוצאות
+  expect(metadataUrls.length).toBe(3);
   expect(metadataUrls[0]).toContain("https://www.google.com");
   expect(metadataUrls[1]).toContain("https://www.facebook.com");
   expect(metadataUrls[2]).toContain("https://www.twitter.com");
 
-  expect(metadataUrls.length).toBe(3); // לבדוק אם יש 3 תוצאות
+  expect(metadataUrls.length).toBe(3);
   expect(metadataUrls[0]).toEqual("1. https://www.google.com");
   expect(metadataUrls[1]).toEqual("2. https://www.facebook.com");
   expect(metadataUrls[2]).toEqual("3. https://www.twitter.com");
 
-  // בדיקת שהשדות ריקים לאחר ההגשה
   // reset
   const url1 = await page.inputValue('input[name="url1"]');
   const url2 = await page.inputValue('input[name="url2"]');
@@ -58,11 +52,9 @@ test("form submission displays results in the correct order", async ({
     );
     const image = await card.$(".metadata-image");
 
-    // וידוא שהכרטיס מכיל כותרת ותיאור
     expect(title).not.toBeNull();
     expect(description).not.toBeNull();
 
-    // בדיקת אם יש תמונה בכרטיס - אם כן, לוודא שהיא קיימת
     if (image) {
       const imageUrl = await image.getAttribute("src");
       expect(imageUrl).not.toBeNull();
